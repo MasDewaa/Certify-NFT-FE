d1db8ba2f11a795c348426b5f9987c516ec65d66
GPadaka19 <gustipadaka19@gmail.com>
2025-06-16 17:45:54 +0700

    chore: initial project setup for Certify-NFT-FE

---
+++ update-prod.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Switch to prod branch
+git checkout prod
+
+# Reset prod to match dev
+git reset --hard dev
+
+# Force push to remote prod
+git push -f origin prod
+
+echo "Production branch has been updated successfully!" 
